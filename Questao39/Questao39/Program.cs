a2c7b2ba84f4d6bbc33a8afb36e8793fafe5391d
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-12-17 17:09:02 -0300

    COnclusão até a questão 36
    Conclusão de todas as atividades até a questão 36

---
+++ Questao39/Questao39/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Questao39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicio:
+            Console.Write("Digite uma idade: ");
+            int idade = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            // condicional if else
+            if (idade < 18)
+            {
+                Console.WriteLine("Menor de idade");
+            } else
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("1 -Voltar para o início.");
+            Console.WriteLine("2 - Encerrar o programa.");
+            Console.Write("\nDigite a opção que deseja: ");
+            int opcao = Convert.ToInt16(Console.ReadLine());
+            // condicional switch case
+            switch (opcao)
+            {
+                case 1:
+                    goto Inicio;
+
+                case 2:
+                    break;
+            }
+        }
+    }
+}
