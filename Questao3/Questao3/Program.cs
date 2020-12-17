0ff9b9f0df16faab1d61532ce02caadba6aa0461
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-12-17 14:22:07 -0300

    Concluido até a questão 22

---
+++ Questao3/Questao3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o salário atual do funcionário: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de reajuste (com separação por vírgula): ");
+            double reajuste = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            double novoSalario = salario + (salario * reajuste);
+            Console.WriteLine($"Salário final do funcionário: {novoSalario}");
+        }
+    }
+}
